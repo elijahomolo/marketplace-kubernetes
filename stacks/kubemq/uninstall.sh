37a4081c4d304393406e3b206b6ef0d21b9e9c97
elijahomolo <eomolo11@gmail.com>
2021-02-12 00:02:30 -0600

    kubemq

--- stacks/kubemq/uninstall.sh
+++ stacks/kubemq/uninstall.sh
@@ -6,15 +6,14 @@ set -e
 # Uninstall objects, namespace and delete CRDs
 ################################################################################
 NAMESPACE="kubemq"
-declare -a CRD=("kubemqclusters.core.k8s.kubemq.io" \
-"kubemqconnectors.core.k8s.kubemq.io" "kubemqdashboards.core.k8s.kubemq.io")
-declare -a clusterrole=("kubemq-operator")
-declare -a role=("kubemq-cluster")
-declare -a clusterrolebinding=("kubemq-operator-kubemq-crb")
+declare -a clusterrole=($(kubectl get clusterrole --no-headers -o custom-columns=":metadata.name" | grep kubemq))
+declare -a crd=($(kubectl get crd --no-headers -o custom-columns=":metadata.name" | grep kubemq))
+declare -a role=($(kubectl get role --no-headers -o custom-columns=":metadata.name" | grep kubemq))
+declare -a clusterrolebinding=($(kubectl get clusterrolebinding --no-headers -o custom-columns=":metadata.name" | grep kubemq))
 
 kubectl delete all --all -n ${NAMESPACE} && kubectl delete ns $NAMESPACE \
- && for i in "${CRD[@]}"; do kubectl delete crd $i ; done && for i in \
- "${clusterrole[@]}"; do kubectl delete clusterrole $i ; done &&  for i in \
- "${role[@]}"; do kubectl delete role $i; done && for i in \
- "${clusterrolebinding[@]}"; do kubectl delete clusterrolebinding $i ;\
+ && for i in "${crd[@]}"; do kubectl delete crd "$i"; ; done && for i in \
+ "${clusterrole[@]}"; do kubectl delete clusterrole "$i" ; done &&  for i in \
+ "${role[@]}"; do kubectl delete role "$i"; done && for i in \
+ "${clusterrolebinding[@]}"; do kubectl delete clusterrolebinding "$i"; \
  done
